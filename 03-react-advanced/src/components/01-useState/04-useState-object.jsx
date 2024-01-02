aaff60edf2d18eae8ab6ef1e05f43afc12fb59a8
Rychy248 <jorgeajrhh@gmail.com>
2024-01-01 20:51:29 -0600

    useEffect, finished

--- 03-react-advanced/src/components/01-useState/04-useState-object.jsx
+++ 03-react-advanced/src/components/01-useState/04-useState-object.jsx
@@ -1,10 +1,36 @@
-import { BlackContainer } from '../Containers';
+// third modules
 import { useState } from 'react';
+// local modules
+import { BlackContainer } from '../Containers';
 
 function UseStateObject({ id }) {
+  const Person = { name:"", age:0, hobby:"" };
+  let [person, setPerson] = useState({...Person});
+  let [secondPerson, setSecondPerson] = useState({...Person});
+
+  function changeHandler({ target:{ id, value} }) {
+    setSecondPerson(prevPerson => ({ ...prevPerson, [id.match(/-(\w+)$/)[1]]:value } ));
+  };
+
+  function changePeople() {
+    const temPerson = {...person}; // the correct way is using a temporary var to save the personData
+    setPerson(secondPerson);
+    setSecondPerson(temPerson);
+  };
+
   return(
     <BlackContainer specialTitle={"Part 1, UseState object"} id={id}>
       <h2>useState object example</h2>
+      <h3>Person{person.name.length > 0 && ` "${person.name}"`}</h3>
+      <p>{person.name} Details:</p>
+      <p>Name: {person.name}</p>
+      <p>Age: {person.age}</p>
+      <p>Hobby: {person.hobby}</p>
+      
+      <input id='useState04-object-name' type='text' value={secondPerson.name} onChange={changeHandler}></input>
+      <input id='useState04-object-age' type='number' value={secondPerson.age} onChange={changeHandler}></input>
+      <input id='useState04-object-hobby' type='text' value={secondPerson.hobby} onChange={changeHandler}></input>
+      <button onClick={changePeople}>Change person</button>
     </BlackContainer>
   );
 };
--- 03-react-advanced/src/components/01-useState/05-useState-gotcha.jsx
+++ 03-react-advanced/src/components/01-useState/05-useState-gotcha.jsx
@@ -1,9 +1,66 @@
+// third modules
+import { useState, useEffect } from "react";
+// local modules
 import { WhiteContainer } from "../Containers";
+import Final from "./final/05-useState-gotcha"
 
 function UseStateGotcha({ id }) {
+  /*
+  * The "functional update", it's mean the dispatch returned by the useState,
+  * can recive a function the "functional update" instance of a single value,
+  * This function get as a parameter the latest state value, of relationated value
+  * in this function, the callback passed to the "setCounter", recive the latest value of "counter"
+  */
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+  const [incrementerPressCounter, setIncrementerPressCounter] = useState(0);
+  const [componentState, setComponentState] = useState("Loading");
+ 
+  console.log("--------- part1, 05 useState-gotcha");
+  useEffect(()=>{ setComponentState("Loaded"); },[]); // update the component state,whe it's full loaded
+
+  function counterUpdater(idCounter) {
+    setIncrementerPressCounter(prevState =>{
+      setComponentState( prevState > 1 ? "Loading" : "Loaded");
+      return prevState -1
+    });
+
+    idCounter === "counter"
+      ? setCounter(latestCounter => (latestCounter + 1)) /*lateste value*/
+      : setCounter2(counter2 + 1) /*is not the latest value*/
+    ;
+
+  };
+  
+  function counterTimeManager (timeToWait, idCounter){
+    setTimeout(()=>{
+      timeToWait > 0
+        ? counterTimeManager((timeToWait -1), idCounter)
+        : counterUpdater(idCounter)
+      ;
+    }, 1000);
+  };
+
+  const incrementer = ({ target:{ id, value}}, timeToWait=3) => {
+    const idCounter = id.match(/-(\w+)$/)[1];
+    setIncrementerPressCounter(latestState => latestState +1)
+    setComponentState("Loading");
+    counterTimeManager(timeToWait, idCounter);
+  };
+  
+
   return(
     <WhiteContainer specialTitle={"Part 1, useState Gotcha"} id={id}>
       <h2>useState "gotcha"</h2>
+      <h6>Look at the console</h6>
+      <h1>Counter: {counter}, Counter2: {counter2}</h1>
+  
+      <button id="part1-useState-gotcha-counter" onClick={incrementer} >Increse counter</button>
+      <button id="part1-useState-gotcha-counter2" onClick={incrementer} >Increse counter2</button>
+  
+      {/* animate-fade-in-out, its a class to show momentanealy the componente */}
+      {(componentState === "Loading") && <h2 className="animate-fade">Waiting...</h2>}
+      
     </WhiteContainer>
   );
 };
